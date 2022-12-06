aa59fb5d8e2cc22b3516234ace25e2aa17f6e938
Prateek Shah <prateekshah@Prateeks-MacBook-Pro.local>
2022-12-06 21:55:07 +0530

    Add code and test for checking palindrome of a number

---
+++ src/main/java/org/dsa/math/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package org.dsa.math;
+
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int number) {
+        if (number >= 0 && number <= 9) return true;
+
+        return number == reverse(number);
+    }
+
+    private static int reverse(int number) {
+        int reverse = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            reverse = reverse*10 + digit;
+            number = number/10;
+        }
+
+        return reverse;
+    }
+}
---
+++ src/test/java/org/dsa/math/PalindromeNumberTest.java
@@ -0,0 +1,39 @@
+package org.dsa.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeNumberTest {
+
+    @Test
+    void shouldReturnTrueGivenASingleDigitNumber() {
+        assertTrue(PalindromeNumber.isPalindrome(0));
+        assertTrue(PalindromeNumber.isPalindrome(8));
+        assertTrue(PalindromeNumber.isPalindrome(7));
+    }
+
+    @Test
+    void shouldReturnTrueGivenAPalindromeNumberWithOddLength() {
+        assertTrue(PalindromeNumber.isPalindrome(121));
+        assertTrue(PalindromeNumber.isPalindrome(55555));
+        assertTrue(PalindromeNumber.isPalindrome(85258));
+        assertTrue(PalindromeNumber.isPalindrome(9874789));
+    }
+
+    @Test
+    void shouldReturnTrueGivenAPalindromeNumberWithEvenLength() {
+        assertTrue(PalindromeNumber.isPalindrome(1221));
+        assertTrue(PalindromeNumber.isPalindrome(555555));
+        assertTrue(PalindromeNumber.isPalindrome(852258));
+        assertTrue(PalindromeNumber.isPalindrome(98744789));
+    }
+
+    @Test
+    void shouldReturnFalseGivenANonPalindromeNumber() {
+        assertFalse(PalindromeNumber.isPalindrome(654));
+        assertFalse(PalindromeNumber.isPalindrome(874651));
+        assertFalse(PalindromeNumber.isPalindrome(5151));
+        assertFalse(PalindromeNumber.isPalindrome(123456));
+    }
+}
