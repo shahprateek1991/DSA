56ad120a0f103d5ea004f395990e6ae1c4d10a3f
Prateek Shah <prateekshah@Prateeks-MacBook-Pro.local>
2022-12-06 17:36:59 +0530

    Add code and test for counting digits in a number

---
+++ src/main/java/org/dsa/math/DigitCounter.java
@@ -0,0 +1,16 @@
+package org.dsa.math;
+
+public class DigitCounter {
+
+    public static int counter(int number) {
+        if (number == 0) return 1;
+
+        int count = 0;
+        while (number > 0) {
+            number = number/10;
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ src/test/java/org/dsa/math/DigitCounterTest.java
@@ -0,0 +1,21 @@
+package org.dsa.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigitCounterTest {
+
+    @Test
+    void shouldReturnOneGivenZero() {
+        assertEquals(1, DigitCounter.counter(0));
+    }
+
+    @Test
+    void shouldReturnLengthGivenAnInteger() {
+        assertEquals(3, DigitCounter.counter(123));
+        assertEquals(2, DigitCounter.counter(10));
+        assertEquals(1, DigitCounter.counter(9));
+        assertEquals(5, DigitCounter.counter(85472));
+    }
+}
