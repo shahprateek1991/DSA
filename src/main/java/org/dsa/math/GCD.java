070ac6f2f58aee16437804cfcfce6329ce01f4a1
Prateek Shah <prateekshah@Prateeks-MacBook-Pro.local>
2022-12-07 09:01:31 +0530

    Add code and test for calculate the GCD of two numbers

---
+++ src/main/java/org/dsa/math/GCD.java
@@ -0,0 +1,30 @@
+package org.dsa.math;
+
+public class GCD {
+
+    public static int gcdNaive(int a, int b) {
+        int gcd = Math.min(a, b);
+
+        while (gcd > 0) {
+            if (a%gcd == 0 && b%gcd == 0) break;
+            gcd--;
+        }
+
+        return gcd;
+    }
+
+    public static int euclideanGCD(int a, int b) {
+        while (a != b) {
+            if (a > b)
+                a = a-b;
+            else
+                b = b-a;
+        }
+        return a;
+    }
+
+    public static int euclideanOptimizedGCD(int a, int b) {
+        if (b == 0) return a;
+        else return euclideanOptimizedGCD(b, a%b);
+    }
+}
---
+++ src/test/java/org/dsa/math/GCDTest.java
@@ -0,0 +1,59 @@
+package org.dsa.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GCDTest {
+
+    @Test
+    void shouldReturnOneAsGCDGivenTwoNumbersDoesNotHaveCommonFactors_UsingNaiveMethod() {
+        assertEquals(1, GCD.gcdNaive(3,5));
+        assertEquals(1, GCD.gcdNaive(9,10));
+        assertEquals(1, GCD.gcdNaive(13,7));
+        assertEquals(1, GCD.gcdNaive(1,1));
+        assertEquals(1, GCD.gcdNaive(1,2));
+    }
+
+    @Test
+    void shouldReturnGCDGivenTwoNumbersHaveCommonFactors_UsingNaiveMethod() {
+        assertEquals(2, GCD.gcdNaive(4,2));
+        assertEquals(5, GCD.gcdNaive(5,10));
+        assertEquals(7, GCD.gcdNaive(28,21));
+        assertEquals(16, GCD.gcdNaive(16,64));
+    }
+
+    @Test
+    void shouldReturnOneAsGCDGivenTwoNumbersDoesNotHaveCommonFactors_UsingEuclideanMethod() {
+        assertEquals(1, GCD.euclideanGCD(3,5));
+        assertEquals(1, GCD.euclideanGCD(9,10));
+        assertEquals(1, GCD.euclideanGCD(13,7));
+        assertEquals(1, GCD.euclideanGCD(1,1));
+        assertEquals(1, GCD.euclideanGCD(1,2));
+    }
+
+    @Test
+    void shouldReturnGCDGivenTwoNumbersHaveCommonFactors_UsingEuclideanMethod() {
+        assertEquals(2, GCD.euclideanGCD(4,2));
+        assertEquals(5, GCD.euclideanGCD(5,10));
+        assertEquals(7, GCD.euclideanGCD(28,21));
+        assertEquals(16, GCD.euclideanGCD(16,64));
+    }
+
+    @Test
+    void shouldReturnOneAsGCDGivenTwoNumbersDoesNotHaveCommonFactors_UsingEuclideanOptimizedMethod() {
+        assertEquals(1, GCD.euclideanOptimizedGCD(3,5));
+        assertEquals(1, GCD.euclideanOptimizedGCD(9,10));
+        assertEquals(1, GCD.euclideanOptimizedGCD(13,7));
+        assertEquals(1, GCD.euclideanOptimizedGCD(1,1));
+        assertEquals(1, GCD.euclideanOptimizedGCD(1,2));
+    }
+
+    @Test
+    void shouldReturnGCDGivenTwoNumbersHaveCommonFactors_UsingEuclideanOptimizedMethod() {
+        assertEquals(2, GCD.euclideanOptimizedGCD(4,2));
+        assertEquals(5, GCD.euclideanOptimizedGCD(5,10));
+        assertEquals(7, GCD.euclideanOptimizedGCD(28,21));
+        assertEquals(16, GCD.euclideanOptimizedGCD(16,64));
+    }
+}
