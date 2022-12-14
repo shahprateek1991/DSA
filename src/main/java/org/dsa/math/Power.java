decdb8af94c7e633f4cf826f6a267e05eba65309
Prateek Shah <prateekshah@Prateeks-MacBook-Pro.local>
2022-12-14 10:12:39 +0530

    Add code and test to calculate powers

--- src/main/java/org/dsa/math/Power.java
+++ src/main/java/org/dsa/math/Power.java
@@ -1,2 +1,27 @@
-package org.dsa.math;public class Power {
+package org.dsa.math;
+
+public class Power {
+
+    public static long exponentRecursive(int x, int n) {
+        long result = 1;
+        while (n > 0) {
+            if (n % 2 != 0) {
+                result = result*x;
+            }
+            n = n/2;
+            x = x*x;
+        }
+        return result;
+    }
+
+    public static long exponentIterative(int x, int n) {
+        if (n == 0) return 1;
+
+        if (n%2 == 0) {
+                long i = exponentRecursive(x, n/2);
+                return i*i;
+        } else {
+            return exponentRecursive(x, n-1) * x;
+        }
+    }
 }
--- src/test/java/org/dsa/math/PowerTest.java
+++ src/test/java/org/dsa/math/PowerTest.java
@@ -1,4 +1,31 @@
+package org.dsa.math;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PowerTest {
-  
+
+    @Test
+    void shouldReturnExponentOfAGivenNumber() {
+        assertEquals(9, Power.exponentIterative(3, 2));
+        assertEquals(1, Power.exponentIterative(3, 0));
+        assertEquals(16, Power.exponentIterative(2, 4));
+        assertEquals(8, Power.exponentIterative(2, 3));
+        assertEquals(343, Power.exponentIterative(7, 3));
+    }
+
+    @Test
+    void shouldReturnExponentOfAGivenNumber_usingRecursive() {
+        assertEquals(9, Power.exponentRecursive(3, 2));
+        assertEquals(1, Power.exponentRecursive(3, 0));
+        assertEquals(16, Power.exponentRecursive(2, 4));
+        assertEquals(8, Power.exponentRecursive(2, 3));
+        assertEquals(343, Power.exponentRecursive(7, 3));
+    }
+
+    @Test
+    void name() {
+        assertEquals(1, Power.myPow(2.00000, -2147483648));
+    }
 }
