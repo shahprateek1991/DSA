50179390222e17720c18256a93a95f5d11ff9593
Prateek Shah <prateekshah@Prateeks-MacBook-Pro.local>
2022-12-15 19:47:31 +0530

    Add code related to basic array operations

--- src/main/java/org/dsa/math/Array.java
+++ src/main/java/org/dsa/math/Array.java
@@ -1,5 +1,7 @@
 package org.dsa.math;
 
+import java.util.Arrays;
+
 public class Array {
 
     public static int findSecondLargestElementIndex(int []arr) {
@@ -28,10 +30,61 @@ public class Array {
     }
 
     public static int[] reverse(int []arr) {
-        for (int i = 0; i<arr.length/2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length-1-i];
-            arr[arr.length-1-i] = temp;
+        return reverse(arr, 0, arr.length-1);
+    }
+
+    public static int[] removeDuplicateFromSortedArray(int []arr) {
+        int index = 1;
+        for (int i = 1; i<arr.length; i++) {
+            if (arr[i-1] != arr[i]) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(arr, index);
+    }
+
+    public static int[] moveAllZerosToEnd(int []arr) {
+        int index = 0;
+        for (int i = 0; i<arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+                index++;
+            }
+        }
+        return arr;
+    }
+
+    public static int[] rotateLeftArray(int []arr, int rotateFactor) {
+        int[] newArr = new int[rotateFactor];
+        System.arraycopy(arr, 0, newArr, 0, rotateFactor);
+        for (int i = rotateFactor; i<arr.length; i++) {
+            arr[i-rotateFactor] = arr[i];
+        }
+        int index = 0;
+        for (int i = arr.length-rotateFactor; i<arr.length; i++) {
+            arr[i] = newArr[index];
+            index++;
+        }
+        return arr;
+    }
+
+    public static int[] rotateLeftArrayWithoutExtraSpace(int []arr, int rotateFactor) {
+        arr = reverse(arr, 0, rotateFactor-1);
+        arr = reverse(arr, rotateFactor, arr.length-1);
+        return reverse(arr, 0, arr.length-1);
+    }
+
+    private static int[] reverse(int[] arr, int low, int high) {
+        while (low < high) {
+            int temp = arr[low];
+            arr[low]  = arr[high];
+            arr[high] = temp;
+
+            low++;
+            high--;
         }
 
         return arr;
--- src/test/java/org/dsa/math/ArrayTest.java
+++ src/test/java/org/dsa/math/ArrayTest.java
@@ -27,4 +27,34 @@ class ArrayTest {
         assertArrayEquals(new int[] {5,4,3,2,1}, Array.reverse(new int[] {1,2,3,4,5}));
         assertArrayEquals(new int[] {4,3,2,1}, Array.reverse(new int[] {1,2,3,4}));
     }
+
+    @Test
+    void shouldRemoveDuplicateFromSortedArray() {
+        assertArrayEquals(new int[] {10,20,30}, Array.removeDuplicateFromSortedArray(new int[]{10,20,20,30,30,30,30}));
+        assertArrayEquals(new int[] {10}, Array.removeDuplicateFromSortedArray(new int[]{10,10,10}));
+
+    }
+
+    @Test
+    void shouldRMoveAllZeroToEndInTheGiveArray() {
+        assertArrayEquals(new int[] {1,3,5,0,0}, Array.moveAllZerosToEnd(new int[] {1,0,3,0,5}));
+        assertArrayEquals(new int[] {3,4,0,0}, Array.moveAllZerosToEnd(new int[] {0,0,3,4}));
+        assertArrayEquals(new int[] {10,0,0,0,0}, Array.moveAllZerosToEnd(new int[] {0,0,0,10,0}));
+        assertArrayEquals(new int[] {10,20}, Array.moveAllZerosToEnd(new int[] {10,20}));
+        assertArrayEquals(new int[] {8,5,10,20,0,0}, Array.moveAllZerosToEnd(new int[] {8,5,0,10,0,20}));
+    }
+
+    @Test
+    void shouldRotateArrayByRotateFactor() {
+        assertArrayEquals(new int[] {2,3,4,5,1}, Array.rotateLeftArray(new int[]{1,2,3,4,5}, 1));
+        assertArrayEquals(new int[] {3,4,5,1,2}, Array.rotateLeftArray(new int[]{1,2,3,4,5}, 2));
+        assertArrayEquals(new int[] {15,10,5,30}, Array.rotateLeftArray(new int[]{10,5,30,15}, 3));
+    }
+
+    @Test
+    void shouldRotateArrayByRotateFactor_withoutUsingExtraSpace() {
+        assertArrayEquals(new int[] {2,3,4,5,1}, Array.rotateLeftArrayWithoutExtraSpace(new int[]{1,2,3,4,5}, 1));
+        assertArrayEquals(new int[] {3,4,5,1,2}, Array.rotateLeftArrayWithoutExtraSpace(new int[]{1,2,3,4,5}, 2));
+        assertArrayEquals(new int[] {15,10,5,30}, Array.rotateLeftArrayWithoutExtraSpace(new int[]{10,5,30,15}, 3));
+    }
 }
