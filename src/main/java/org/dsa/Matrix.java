6b94f148bddcc971a0b7f0f1a07f7741e2ec3f6e
prateekshah <shah.prateek1991@gmail.com>
2023-01-08 16:35:38 +0530

    Matrix traversal in snake pattern

--- src/main/java/org/dsa/Matrix.java
+++ src/main/java/org/dsa/Matrix.java
@@ -1,2 +1,24 @@
-package org.dsa;public class Matrix {
+package org.dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matrix {
+
+    public static List<Integer> snakeTraversal(int[][] arr) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (i%2 == 0) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    result.add(arr[i][j]);
+                }
+            } else {
+                for (int j = arr[i].length-1; j >=0; j--) {
+                    result.add(arr[i][j]);
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/org/dsa/MatrixTest.java
+++ src/test/java/org/dsa/MatrixTest.java
@@ -1,4 +1,18 @@
+package org.dsa;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MatrixTest {
-  
+
+    @Test
+    void shouldTraverseMatrixInSnakePattern() {
+        assertEquals(List.of(1,2,3), Matrix.snakeTraversal(new int[][]{{1,2,3}}));
+        assertEquals(List.of(1,2,3,6,5,4,7,8,9), Matrix.snakeTraversal(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+        assertEquals(List.of(1,2,3,6,5,4,7,8,9,12,11,10),
+                Matrix.snakeTraversal(new int[][]{{1,2,3},{4,5,6},{7,8,9},{10,11,12}}));
+    }
 }
